199d811510979039b9b7cd2929f23264d54f2162
pcast <pcast@DESK-E>
2023-08-26 21:05:58 -0600

    simulador de registro con random access file

---
+++ src/sequence/datos_registro.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sequence;
+
+/**
+ *
+ * @author pcast
+ */
+public class datos_registro {
+    private int id;
+    private String nombre;
+    private int puntos;
+
+    public datos_registro(int id, String nombre, int puntos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puntos=puntos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos= puntos;
+    }
+
+
+
+}
